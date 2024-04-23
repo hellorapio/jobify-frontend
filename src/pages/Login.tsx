1fde5424ef30accb86f25f3e4e261766f9043095
Anubis <anubis356@outlook.com>
2024-04-23 06:23:33 +0200

    feat: back logo

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import Logo from "@/components/shared/Logo";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -9,6 +10,15 @@ export function Login() {
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
+            <div className="">
+              <Link
+                to={"/"}
+                className="flex justify-center items-center gap-2"
+              >
+                <span className="text-2xl">&larr;</span>
+                <Logo />
+              </Link>
+            </div>
             <h1 className="text-3xl font-bold">Login</h1>
             <p className="text-balance text-muted-foreground">
               Enter your email below to login to your account
