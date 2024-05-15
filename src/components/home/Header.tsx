3877c7502b0d1d4b0f33bf351d27865a109f15d4
Anubis <anubis356@outlook.com>
2024-05-15 16:24:40 +0300

    removing blurry background

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -9,10 +9,7 @@ export default function Header() {
     <header className=" h-[calc(100vh-64px)] border-b-[6px] border-primary">
       <div
         className="container flex flex-col justify-center text-center items-center md:-translate-y-10 h-full
-      before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full 
-      before:bg-gradient-radial before:from-primary before:to-primary before:blur-2xl before:content-['']  
-      before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-primary before:dark:opacity-20  
-      sm:before:w-[480px] before:lg:h-[360px]"
+      "
       >
         <HeroText />
         <div className="flex gap-6">
