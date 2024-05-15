b338bf4baf1acc9232088e8af6ee4f7af0d7f3c8
Anubis <anubis356@outlook.com>
2024-05-15 16:21:22 +0300

    fix

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -10,13 +10,9 @@ export default function Header() {
       <div
         className="container flex flex-col justify-center text-center items-center md:-translate-y-10 h-full
       before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full 
-      before:bg-gradient-radial before:from-primary before:to-primary before:blur-2xl before:content-[''] 
-      after:absolute after:-z-20 after:h-[180px] after:w-fit after:translate-x-1/3 
-      after:bg-gradient-conic after:from-primary after:via-primary after:blur-2xl 
-      after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent
-       before:dark:to-primary before:dark:opacity-20 after:dark:from-primary 
-       after:dark:via-primary after:dark:opacity-100 sm:before:w-[480px] 
-       sm:after:w-[240px] before:lg:h-[360px]"
+      before:bg-gradient-radial before:from-primary before:to-primary before:blur-2xl before:content-['']  
+      before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-primary before:dark:opacity-20  
+      sm:before:w-[480px] before:lg:h-[360px]"
       >
         <HeroText />
         <div className="flex gap-6">
